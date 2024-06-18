32ad229f7444c2913ac62031f5f137202ce440cd
Juan Aguilar <juan.aguilarl@unmsm.edu.pe>
2024-06-17 23:05:55 -0500

    añadida función para modificar habitación autenticación por contraseña funcional

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <locale.h>
 #include <string.h>
 
 using namespace std;
@@ -27,18 +29,21 @@ struct habitacion
 };
 void admin(habitacion (&)[8][4]);
 void aniadir_habitacion(habitacion (&)[8][4]);
+void ver_habitaciones_disponibles(habitacion (&)[8][4]);
+void modificar_habitacion(habitacion (&)[8][4]);
 int main()
 {
+    setlocale(LC_ALL, "es_PE");
     habitacion hotel[8][4];
     char passwd[15] = {'\000'}, p[15] = {'\000'};
     int op = 1;
-    cout << "¡Bienvenido!";
+    cout << "¡Bienvenido!\n";
     while (op)
     {
         cout << "---------------------------------------------\n";
         cout << "1. Ver habitaciones disponibles\n";
-        cout << "2. Registrar huésped\n";
-        cout << "3. Registrar salida de huésped\n";
+        cout << "2. Registrar huesped\n";
+        cout << "3. Registrar salida de huesped\n";
         cout << "4. Interfaz administrativa\n";
         cout << "0. Salir\n";
         cout << "---------------------------------------------\n";
@@ -47,24 +52,7 @@ int main()
         switch (op)
         {
         case 1:
-            for (int i = 0; i < 8; i++)
-            {
-                int e = 0;
-                cout << i + 1 << "° piso: \n";
-                for (int j = 0; j < 4; j++)
-                {
-                    if (hotel[i][j].huespdesActuales < hotel[i][j].capacidadMax)
-                    {
-                        cout << "Habitacion " << hotel[i][j].codigo << "\n";
-                        cout << "Tipo: " << hotel[i][j].tipo << '\n'; // TODO: implementar switch
-                        e++;
-                    }
-                }
-                if (e == 0)
-                {
-                    cout << "No hay habitaciones disponibles en este piso.\n";
-                }
-            }
+            ver_habitaciones_disponibles(hotel);
             break;
         case 2:
             /* code */
@@ -86,6 +74,7 @@ int main()
                 while (strcmp(passwd, p) != 0)
                 {
                     cout << "Ingrese contrasena: ";
+                    fflush(stdin);
                     gets(p);
                     if (strcmp(p, passwd) == 0)
                     {
@@ -107,6 +96,75 @@ int main()
         }
     }
 }
+void ver_habitaciones_disponibles(habitacion (&hotel)[8][4])
+{
+    for (int i = 0; i < 8; i++)
+    {
+        int e = 0;
+        cout << i + 1 << "° piso: \n";
+        for (int j = 0; j < 4; j++)
+        {
+            if (hotel[i][j].huespdesActuales < hotel[i][j].capacidadMax)
+            {
+                cout << "Habitacion " << hotel[i][j].codigo << "\n";
+                cout << "Tipo: ";
+                switch (hotel[i][j].tipo)
+                {
+                case 's':
+                case 'S':
+                    cout << "Simple\n";
+                    break;
+                case 'd':
+                case 'D':
+                    cout << "Doble\n";
+                    break;
+                default:
+                    cout << "Desconocido\n";
+                    break;
+                }
+                cout << "Capacidad máxima: " << hotel[i][j].capacidadMax << "\n";
+                cout << "Precio por noche: " << hotel[i][j].precioNoche << "\n";
+                e++;
+                cout << endl;
+            }
+        }
+        if (e == 0)
+        {
+            cout << "No hay habitaciones disponibles en este piso.\n";
+        }
+    }
+}
+void admin(habitacion (&hotel)[8][4])
+{
+    int op = 1;
+    while (op)
+    {
+        cout << "Bienvenido\n";
+        cout << "---------------------------------------------\n";
+        cout << "1. Añadir habitacion\n";
+        cout << "2. Colocar habitación fuera de servicio\n";
+        cout << "3. Editar habitacion\n";
+        cout << "0. Regresar a la interfaz principal\n";
+        cout << "---------------------------------------------\n";
+        cout << "-> ";
+        cin >> op;
+        switch (op)
+        {
+        case 1:
+            aniadir_habitacion(hotel);
+            break;
+        case 3:
+            modificar_habitacion(hotel);
+            break;
+        case 0:
+            cout << "Regresando a la interfaz principal\n";
+            break;
+        default:
+            cout << "Operacion no valida, intente de nuevo\n";
+            break;
+        }
+    }
+};
 void aniadir_habitacion(habitacion (&hotel)[8][4])
 {
     int piso, numero;
@@ -132,7 +190,7 @@ void aniadir_habitacion(habitacion (&hotel)[8][4])
         else if (tipo == 'd' || tipo == 'D')
         {
             hotel[piso - 1][numero - 1].capacidadMax = 2;
-        } 
+        }
         cout << "Habitacion creada exitosamente\n";
     }
     else
@@ -140,31 +198,35 @@ void aniadir_habitacion(habitacion (&hotel)[8][4])
         cout << "Error: la habitacion " << piso * 100 + numero << " ya existe\n";
     }
 }
-void admin(habitacion (&hotel)[8][4])
+void modificar_habitacion(habitacion (&hotel)[8][4])
 {
-    int op = 1;
-    while (op)
+    int piso, numero;
+    cout << "Ingrese el piso de la habitacion a modificar: ";
+    cin >> piso;
+    cout << "Ingrese el número de la habitacion a modificar: ";
+    cin >> numero;
+    if (hotel[piso - 1][numero - 1].codigo != 0)
     {
-        cout << "Bienvenido\n";
-        cout << "---------------------------------------------\n";
-        cout << "1. Añadir habitación\n";
-        cout << "2. Colocar habitación fuera de servicio\n";
-        cout << "3. Editar habitación\n";
-        cout << "0. Regresar a la interfaz principal\n";
-        cout << "---------------------------------------------\n";
-        cout << "-> ";
-        cin >> op;
-        switch (op)
+        char tipo;
+        float precio;
+        cout << "Precio por noche para esta habitacion: ";
+        cin >> precio;
+        cout << "Ingrese tipo de habitacion ((s)imple, (d)oble): ";
+        cin >> tipo;
+        hotel[piso - 1][numero - 1].precioNoche = precio;
+        hotel[piso - 1][numero - 1].tipo = tipo;
+        if (tipo == 's' || tipo == 'S')
         {
-        case 1:
-            aniadir_habitacion(hotel);
-            break;
-        case 0:
-            cout << "Regresando a la interfaz principal\n";
-            break;
-        default:
-        cout << "Operacion no valida, intente de nuevo\n";
-            break;
+            hotel[piso - 1][numero - 1].capacidadMax = 1;
         }
+        else if (tipo == 'd' || tipo == 'D')
+        {
+            hotel[piso - 1][numero - 1].capacidadMax = 2;
+        }
+        cout << "Habitacion modificada exitosamente\n";
     }
-};
+    else
+    {
+        cout << "Error: la habitacion " << piso * 100 + numero << " no existe\n";
+    }
+}
